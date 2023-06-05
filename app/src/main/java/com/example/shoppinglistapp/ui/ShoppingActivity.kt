93bc76df11a41bd933d00a0c1cf33647426c4536
anoosm <justgoanoos@gmail.com>
2023-06-05 08:39:29 +0530

    added viewModel class

--- app/src/main/java/com/example/shoppinglistapp/ui/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/ui/ShoppingActivity.kt
@@ -1,11 +1,15 @@
-package com.example.shoppinglistapp
+package com.example.shoppinglistapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.shoppinglistapp.R
+import com.example.shoppinglistapp.ui.shoppinglist.ShoppingViewModel
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+        val viewModel = ViewModelProvider(this).get(ShoppingViewModel::class.java)
     }
 }
